6a610ad6d7d58ebf74fdcd3025c017bc808b3b35
Gustavo Mello <gustavomello@outlook.com>
2022-06-27 23:47:22 -0300

    changed game to button based

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
+const btn = document.querySelectorAll('button');
+btn.forEach(btn => btn.addEventListener('click', event => {
+    playRound(btn.textContent);
+  }));
+
+const tally = document.querySelector('.tally');
+let computerPoints = 0;
+let playerPoints = 0;
+
+
 function computerChose() {
     let i;
     i = Math.floor(Math.random() * 3);
@@ -5,62 +15,60 @@ function computerChose() {
     (i === 1) {return 'Paper'} else if
     (i === 2) {return 'Scissors'};
 }
-function playerChose(){
-    let choice = prompt("Chose Rock, Paper or Scissors");
-    if (choice === null) {return}
-    choice = choice.toLowerCase()
-    if (choice === 'rock') {return 'Rock'} else if
-    (choice === 'paper') {return 'Paper'} else if
-    (choice === 'scissors') {return 'Scissors'};
-}
-function playRound() {
-    let playerSelection = playerChose()
+function playRound(playerSelection) {
     let computerSelection = computerChose()
-    if (playerSelection === undefined){
-        console.log('Player dumb, computer wins the round');
-        return 'computerPoint'
-    }
-    console.log('Player chose ' + playerSelection)
-    console.log('Computer chose ' + computerSelection)
+    //console.log('Player chose ' + playerSelection + ' and Computer chose ' + computerSelection)
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
-        return 'tie'
+        // console.log("It's a tie!");
+        //return 'tie'
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper' ||
             playerSelection === 'Paper' && computerSelection === 'Scissors' ||
             playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        console.log('Computer wins the round')
-        return 'computerPoint'
+        // console.log('Computer wins the round')
+        computerPoints += 1;
+        //return 'computerPoint'
     } else {
-        console.log('Player wins the round')
-        return 'playerPoint'
+        // console.log('Player wins the round')
+        playerPoints += 1;
+        //return 'playerPoint'
     }
- 
+    tally.textContent = 'Computer ' + computerPoints +  ' vs Player ' + playerPoints;
+    if(computerPoints === 5){
+        tally.textContent = 'Computer has won ' + computerPoints +  ' to ' + playerPoints;
+        computerPoints = 0;
+        playerPoints = 0;
+    }
+    if (playerPoints === 5){
+        tally.textContent = 'Player has won ' + playerPoints +  ' to ' + computerPoints;
+        computerPoints = 0;
+        playerPoints = 0;
+    }
 }
 
-function game(){
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 0; i < 5; i++)
-    {   
-        let roundNumber = i + 1;
-        console.log('Round ' + roundNumber);
-        let round = playRound();
-        if (round === 'tie'){console.log('No points for anyone');
-        } else if (round === 'computerPoint'){
-            computerPoints++;
-        } else if (round === 'playerPoint') {
-            playerPoints++;
-        }
-        console.log('Computer has ' + computerPoints + ' points.');
-        console.log('Player has ' + playerPoints + ' points.')
+// function game(){
+//     let playerPoints = 0;
+//     let computerPoints = 0;
+//     for (let i = 0; i < 5; i++)
+//     {   
+//         let roundNumber = i + 1;
+//         console.log('Round ' + roundNumber);
+//         let round = playRound();
+//         if (round === 'tie'){console.log('No points for anyone');
+//         } else if (round === 'computerPoint'){
+//             computerPoints++;
+//         } else if (round === 'playerPoint') {
+//             playerPoints++;
+//         }
+//         console.log('Computer has ' + computerPoints + ' points.');
+//         console.log('Player has ' + playerPoints + ' points.')
 
-    }
-    if (playerPoints === computerPoints) {console.log("The final winner is... nobody.")
-} else if (playerPoints > computerPoints) {console.log("The final winner is Player")
-} else {console.log("The final winner is Computer! I wish I could say it was a pleasure.")}
+//     }
+//     if (playerPoints === computerPoints) {console.log("The final winner is... nobody.")
+// } else if (playerPoints > computerPoints) {console.log("The final winner is Player")
+// } else {console.log("The final winner is Computer! I wish I could say it was a pleasure.")}
 
-}
-game()
+// }
+// game()
 
 
 
